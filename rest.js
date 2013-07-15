caee63863eb946ec2e162b7fb97af38e287e705a
Anandraj Jagadeesan <anandrajj@gmail.com>
2013-07-15 15:05:59 +0000

    Changed code as per assignment 3

--- rest.js
+++ rest.js
@@ -40,6 +40,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     return fn.bind({});
 };
@@ -49,16 +69,19 @@ if(require.main == module) {
    console.error('Inside the code');
    program
       .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-      .option('-u, --url <url_line>', 'Url link to htlml', clone(assertUrlExists), HTMLURL_DEFAULT)
-     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-u, --url <url_line>', 'Url link to htlml', clone(assertUrlExists))
+     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
       .parse(process.argv);
     if(program.file){
       console.log('Detected file');
+      var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
     }
     if(program.url){
       console.log('Detected Url');
+      checkHtmlUrl(program.url,program.checks);
     }
-    checkHtmlUrl(program.url,program.checks);
-} else {
+}else {
   exports.checkHtmlUrl = checkHtmlUrl;
 }
